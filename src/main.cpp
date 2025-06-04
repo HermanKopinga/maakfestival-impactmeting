5d2466b03cfac89a2d70ddee3906cfea81dde361
Herman Kopinga <herman@kopinga.nl>
2025-06-04 21:33:12 +0200

    Ruwe copy-paste opbouw van hoe het zou kunnen werken. Werkt eerste keer nog niet :)

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,33 @@
 #include <Arduino.h>
 #include <Bounce.h>
+#include <SparkFunSX1509.h>  // Library added manually because that's the platformio way if it is not in the standard catalog?
+#include <Wire.h>
+
+// Multiplexer chip sx1509 stuff.
+const byte SX1509_ADDRESS = 0x3E;  // SX1509 I2C address
+
+const byte SX1509_BUTTON1LED = 14;     // (draining current)
+const byte SX1509_BUTTON1BUTTON = 15;  // 
+const byte SX1509_BUTTON2LED = 12;     // (draining current)
+const byte SX1509_BUTTON2BUTTON = 13;  // 
+const byte SX1509_BUTTON3LED = 10;     // (draining current)
+const byte SX1509_BUTTON3BUTTON = 11;  // 
+const byte SX1509_BUTTON4LED = 8;     // (draining current)
+const byte SX1509_BUTTON4BUTTON = 9;  // 
+const byte SX1509_BUTTON5LED = 6;     // (draining current)
+const byte SX1509_BUTTON5BUTTON = 7;  // 
+const byte SX1509_BUTTON6LED = 4;     // (draining current)
+const byte SX1509_BUTTON6BUTTON = 5;  // 
+const byte SX1509_BUTTON7LED = 2;     // (draining current)
+const byte SX1509_BUTTON7BUTTON = 3;  // 
+const byte SX1509_BUTTON8LED = 0;     // (draining current)
+const byte SX1509_BUTTON8BUTTON = 1;  // 
+
+const byte SX1509_INTERRUPT_PIN = 20;
+
+#define sdaPin 18
+#define sclPin 19
+SX1509 io;  // Create an SX1509 object to be used throughout
 
 Bounce button0 = Bounce(23, 20);
 Bounce button1 = Bounce(22, 20);  // 10 ms debounce time is appropriate
@@ -10,25 +38,158 @@ Bounce button4 = Bounce(19, 20);
 unsigned long lastAction = 0;
 unsigned long currentMillis = 0;
 
+byte buttonChanges = 1;
+
+void processButtons() {
+  buttonChanges = 1;
+}
+
+void multiplexerSetup() {
+  // The SX1509 interrupt is active-low.
+  pinMode(SX1509_INTERRUPT_PIN, INPUT_PULLUP);
+
+  //Wire.begin(sdaPin, sclPin);
+
+  // Call io.begin(<address>) to initialize the SX1509. If it
+  // successfully communicates, it'll return 1.
+  if (io.begin(SX1509_ADDRESS) == false) {
+    Serial.println("Failed to communicate. Check wiring and address of SX1509.");
+    digitalWrite(22, HIGH);  // If we failed to communicate, turn the pin 13 LED on
+    while (1)
+      ;  // If we fail to communicate, loop forever.
+  }
+
+  io.debounceTime(20);  // Set debounce time to 32 ms.
+
+  io.pinMode(SX1509_BUTTON1LED, OUTPUT);
+  io.pinMode(SX1509_BUTTON1BUTTON, INPUT_PULLUP);
+  io.pinMode(SX1509_BUTTON2LED, OUTPUT);
+  io.pinMode(SX1509_BUTTON2BUTTON, INPUT_PULLUP);
+  io.pinMode(SX1509_BUTTON3LED, OUTPUT);
+  io.pinMode(SX1509_BUTTON3BUTTON, INPUT_PULLUP);
+  io.pinMode(SX1509_BUTTON4LED, OUTPUT);
+  io.pinMode(SX1509_BUTTON4BUTTON, INPUT_PULLUP);
+  io.pinMode(SX1509_BUTTON5LED, OUTPUT);
+  io.pinMode(SX1509_BUTTON5BUTTON, INPUT_PULLUP);
+  io.pinMode(SX1509_BUTTON6LED, OUTPUT);
+  io.pinMode(SX1509_BUTTON6BUTTON, INPUT_PULLUP);
+  io.pinMode(SX1509_BUTTON7LED, OUTPUT);
+  io.pinMode(SX1509_BUTTON7BUTTON, INPUT_PULLUP);
+  io.pinMode(SX1509_BUTTON8LED, OUTPUT);
+  io.pinMode(SX1509_BUTTON8BUTTON, INPUT_PULLUP);
+
+
+  io.enableInterrupt(SX1509_BUTTON1BUTTON, CHANGE);
+  io.enableInterrupt(SX1509_BUTTON2BUTTON, CHANGE);
+  io.enableInterrupt(SX1509_BUTTON3BUTTON, CHANGE);
+  io.enableInterrupt(SX1509_BUTTON4BUTTON, CHANGE);
+  io.enableInterrupt(SX1509_BUTTON5BUTTON, CHANGE);
+  io.enableInterrupt(SX1509_BUTTON6BUTTON, CHANGE);
+  io.enableInterrupt(SX1509_BUTTON7BUTTON, CHANGE);
+  io.enableInterrupt(SX1509_BUTTON8BUTTON, CHANGE);
+
+  io.debouncePin(SX1509_BUTTON1BUTTON);  
+  io.debouncePin(SX1509_BUTTON2BUTTON);  
+  io.debouncePin(SX1509_BUTTON3BUTTON);  
+  io.debouncePin(SX1509_BUTTON4BUTTON); 
+  io.debouncePin(SX1509_BUTTON5BUTTON);   
+  io.debouncePin(SX1509_BUTTON6BUTTON);     
+  io.debouncePin(SX1509_BUTTON7BUTTON);   
+  io.debouncePin(SX1509_BUTTON8BUTTON);
+  
+  attachInterrupt(digitalPinToInterrupt(SX1509_INTERRUPT_PIN), processButtons, FALLING);
+
+  // Blink the LED a few times before we start:
+  for (int i = 0; i < 2; i++) {
+    // Use io.digitalWrite(<pin>, <LOW | HIGH>) to set an
+    // SX1509 pin either HIGH or LOW:
+    io.digitalWrite(SX1509_BUTTON7LED, HIGH);
+    io.digitalWrite(SX1509_BUTTON8LED, HIGH);
+    delay(100);
+    io.digitalWrite(SX1509_BUTTON7LED, LOW);
+    io.digitalWrite(SX1509_BUTTON8LED, LOW);
+    delay(100);
+    io.digitalWrite(SX1509_BUTTON7LED, HIGH);
+    io.digitalWrite(SX1509_BUTTON8LED, HIGH);
+  }
+  //io.pinMode(SX1509_WHITELED1, ANALOG_OUTPUT);
+}
+
+
 void setup() {
-  pinMode(23, INPUT_PULLUP);
+  /*pinMode(23, INPUT_PULLUP);
   pinMode(22, INPUT_PULLUP);
   pinMode(21, INPUT_PULLUP);
   pinMode(20, INPUT_PULLUP);
-  pinMode(19, INPUT_PULLUP);
+  pinMode(19, INPUT_PULLUP);*/
   Keyboard.begin();
   pinMode(16, OUTPUT);  
   Serial.begin(115200);
+  while (!Serial);
+  multiplexerSetup();
+  Serial.println("hoi");
 }
+
+void doMultiplexedButtons() {
+  // read io.interruptSource() find out which pin generated
+  // an interrupt and clear the SX1509's interrupt output.
+  unsigned int intStatus = io.interruptSource();
+  // For debugging handiness, print the intStatus variable.
+  // Each bit in intStatus represents a single SX1509 IO.
+  Serial.println("intStatus = " + String(intStatus, BIN));
+
+  if (intStatus & (1 << SX1509_BUTTON1BUTTON)) {
+    Serial.println("Button 1 pressed!");
+    Keyboard.print(currentMillis/10);
+    Keyboard.println(" a");
+  }
+  if (intStatus & (1 << SX1509_BUTTON2BUTTON)) {
+    Serial.println("Button 2 pressed!");
+    Keyboard.print(currentMillis/10);
+    Keyboard.println(" b");
+  }
+  if (intStatus & (1 << SX1509_BUTTON3BUTTON)) {
+    Serial.println("Button 3 pressed!");
+    Keyboard.print(currentMillis/10);
+    Keyboard.println(" c");
+  }
+  if (intStatus & (1 << SX1509_BUTTON4BUTTON)) {
+    Serial.println("Button 4 pressed!");
+    Keyboard.print(currentMillis/10);
+    Keyboard.println(" d");
+  }
+  if (intStatus & (1 << SX1509_BUTTON5BUTTON)) {
+    Serial.println("Button 5 pressed!");
+    Keyboard.print(currentMillis/10);
+    Keyboard.println(" e");
+  }
+  if (intStatus & (1 << SX1509_BUTTON6BUTTON)) {
+    Serial.println("Button 6 pressed!");
+    Keyboard.print(currentMillis/10);
+    Keyboard.println(" f");
+  }
+  if (intStatus & (1 << SX1509_BUTTON7BUTTON)) {
+    Serial.println("Button 7 pressed!");
+    Keyboard.print(currentMillis/10);
+    Keyboard.println(" g");
+  }
+  if (intStatus & (1 << SX1509_BUTTON8BUTTON)) {
+    Serial.println("Button 8 pressed!");
+    Keyboard.print(currentMillis/10);
+    Keyboard.println(" h");
+  }
+
+}
+
 
 void loop() {
   currentMillis = millis();
-  button0.update();
-  button1.update();    
-  button2.update();
-  button3.update();  
-  button4.update();  
-  
+
+  if (buttonChanges) {
+    buttonChanges = 0;
+    doMultiplexedButtons();
+  }
+
   if (currentMillis - lastAction > 1000) {
     analogWrite(16, 0);    
     Serial.println("huh");
@@ -38,33 +199,12 @@ void loop() {
   }
   
   if (button0.fallingEdge()) {
-    Keyboard.print(currentMillis/10);
-    Keyboard.println(" a");
+    //Keyboard.print(currentMillis/10);
+    //Keyboard.println(" a");
     lastAction = currentMillis;
     analogWrite(16, 160);
   }
-  if (button1.fallingEdge()) {
-    Keyboard.print(currentMillis/10);
-    Keyboard.println(" b");
-    lastAction = currentMillis;
-    digitalWrite(16, 160);
-  }
-  if (button2.fallingEdge()) {
-    Keyboard.print(currentMillis/10);
-    Keyboard.println(" c");
-    lastAction = currentMillis;
-    analogWrite(16, 160);
-  }
-  if (button3.fallingEdge()) {
-    Keyboard.print(currentMillis/10);
-    Keyboard.println(" d");
-    lastAction = currentMillis;
-    analogWrite(16, 160);
-  }
-  if (button4.fallingEdge()) {
-    Keyboard.print(currentMillis/10);
-    Keyboard.println(" e");
-    lastAction = currentMillis;
-    analogWrite(16, 160);
-  }
+
 }
+
+
